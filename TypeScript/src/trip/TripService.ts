83a7658b42f4c2457f7c2cc9264cc3ba86cfc02f
harbor.liu <harbor.liu@104.com.tw>
2022-03-30 15:48:10 +0800

    Refactor TripService
    1. Early return when loggedUser is not found.
    2. Early return in the loop.
    3. Reduce unnecessary variables.

--- TypeScript/src/trip/TripService.ts
+++ TypeScript/src/trip/TripService.ts
@@ -13,25 +13,18 @@ export default class TripService {
     }
 
     public getTripsByUser(user: User): Trip[] {
-        let tripList: Trip[] = [];
         const loggedUser: User = this.userSession.getLoggedUser();
-        let isFriend: boolean = false;
 
-        if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
-                tripList = this.tripDao.findTripsByUser(user);
-            }
-
-            return tripList;
-        } else {
+        if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
+        
+        for (const friend of user.getFriends()) {
+            if (friend === loggedUser) {
+                return this.tripDao.findTripsByUser(user);
+            }
+        }
+
+        return [];
     }
 }
